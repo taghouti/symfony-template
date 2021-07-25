95c90cba2e2016f6aece37f8679a2f6a2ee62ea9
Isha <ishadijcks@gmail.com>
2021-07-25 20:30:05 +0200

    Add Member fixtures

---
+++ src/DataFixtures/MemberFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Member;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class MemberFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $member = new Member();
+
+        $member->setPassword($this->passwordHasher->hashPassword(
+            $member,
+            'member'
+        ));
+        $member->setUsername('member');
+        $manager->persist($member);
+
+        $admin = new Member();
+
+        $admin->setPassword($this->passwordHasher->hashPassword(
+            $admin,
+            'admin'
+        ));
+        $admin->setUsername('admin');
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
