0c8ec696fb17e2cc00eb0c4e2ff4008251831d34
Tarek Taghouti <taghoutitarek@gmail.com>
2022-05-27 09:42:15 +0100

    add import

--- src/Controller/Admin/FileCrudController.php
+++ src/Controller/Admin/FileCrudController.php
@@ -3,15 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\File;
+use App\Entity\FileField;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use Symfony\Component\DomCrawler\Field\FileFormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class FileCrudController extends AbstractCrudController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public static function getEntityFqcn(): string
     {
         return File::class;
     }
 
-
+    public function configureFields(string $pageName): iterable
+    {
+        $fields = $this->entityManager->getRepository(FileField::class)->findAll();
+        foreach ($fields as $field) {
+            yield TextField::new($field->getKey())->setLabel($field->getLabel());
+        }
+    }
 }
--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -2,23 +2,33 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\File;
+use App\Entity\FileField;
+use App\Entity\FilePath;
 use App\Entity\Member;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
 
     private $entityManager;
     private $passwordEncoder;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(
+        EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, ParameterBagInterface $parameterBag)
     {
         $this->entityManager = $entityManager;
         $this->passwordEncoder = $passwordEncoder;
+        $this->parameterBag = $parameterBag;
     }
 
     public static function getSubscribedEvents()
@@ -26,6 +36,7 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         return [
             BeforeEntityPersistedEvent::class => ['addMember'],
             BeforeEntityUpdatedEvent::class => ['updateMember'],
+            BeforeEntityPersistedEvent::class => ['addFilePath'],
         ];
     }
 
@@ -49,6 +60,16 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         $this->setPassword($entity);
     }
 
+    public function addFilePath(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof FilePath)) {
+            return;
+        }
+        $this->setPath($entity);
+    }
+
     /**
      * @param Member $entity
      */
@@ -64,6 +85,39 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         );
         $this->entityManager->persist($entity);
         $this->entityManager->flush();
+    }
+
+    /**
+     * @param FilePath $entity
+     */
+    public function setPath(FilePath $entity): void
+    {
+        $path = $entity->getPath();
+        $entity->setPath($path);
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+        if (strtolower($entity->getType()) == 'data') $this->import($path);
+    }
+
+    public function import($path) {
+        $spreadsheet = IOFactory::load(
+            $this->parameterBag->get('kernel.project_dir') . "/public$path"
+        );
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        $query = "INSERT INTO file VALUES ";
+        foreach ($sheetData as $rowIndex => $currentRow)
+        {
+            if ($rowIndex == 1) continue;
+            $query .= " (null";
+            $fields = $this->entityManager->getRepository(FileField::class)->findAll();
+            foreach ($fields as $index => $field) {
+                $query .= ",'" . str_replace("'", "\'", $currentRow[chr($index + 65)]) . "'";
+            }
+            $query .= " ) ";
+        }
+        $this->entityManager->getConnection()->executeUpdate($query);
+        $this->entityManager->flush();
+
     }
 
 }
